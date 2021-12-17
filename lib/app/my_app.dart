f575b3276ac1b62afea945c4548b90ff580ec608
baborges45 <brunoquemsoueu@gmail.com>
2021-12-17 17:56:53 -0300

    structuring the project

---
+++ lib/app/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animated_fab_menu/app/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
