7327e3a355f3d9198f6946c3b517ae8b11f3bdc2
baborges45 <brunoquemsoueu@gmail.com>
2021-12-18 08:40:41 -0300

    adding animation to the button

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,41 +1,177 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_fab_menu/app/components/circular_button.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  late Animation degOneTranslationAnimation,
+      degTwoTranslationAnimation,
+      degThreeTranslationAnimation;
+  late Animation rotationAnimation;
+
+  double getRadiansFromDegree(double degree) {
+    double unitRadian = 57.295779513;
+    return degree / unitRadian;
+  }
+
+  @override
+  void dispose() {
+    animationController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    degOneTranslationAnimation = TweenSequence([
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.2), weight: 75.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.2, end: 1.0), weight: 25.0),
+    ]).animate(animationController!);
+    degTwoTranslationAnimation = TweenSequence([
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.4), weight: 55.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.4, end: 1.0), weight: 45.0),
+    ]).animate(animationController!);
+    degThreeTranslationAnimation = TweenSequence([
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.0, end: 1.75), weight: 35.0),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1.75, end: 1.0), weight: 65.0),
+    ]).animate(animationController!);
+    rotationAnimation = Tween<double>(begin: 180.0, end: 0.0).animate(
+        CurvedAnimation(parent: animationController!, curve: Curves.easeOut));
+    super.initState();
+    animationController!.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Animated'),
-      ),
-      body: Container(
-        width: size.width,
-        height: size.height,
-        child: Stack(
-          children: [
-            Positioned(
+        body: Container(
+      width: size.width,
+      height: size.height,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
               right: 30,
               bottom: 30,
               child: Stack(
-                children: [
-                  CircularButton(
+                alignment: Alignment.bottomRight,
+                children: <Widget>[
+                  IgnorePointer(
+                    child: Container(
+                      color: Colors.transparent,
+                      height: 150.0,
+                      width: 150.0,
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: Offset.fromDirection(getRadiansFromDegree(270),
+                        degOneTranslationAnimation.value * 100),
+                    child: Transform(
+                      transform: Matrix4.rotationZ(
+                          getRadiansFromDegree(rotationAnimation.value))
+                        ..scale(degOneTranslationAnimation.value),
+                      alignment: Alignment.center,
+                      child: CircularButton(
+                        color: Colors.blue,
+                        width: 50,
+                        heigth: 50,
+                        icon: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                        onclick: () {
+                          print('First Button');
+                        },
+                      ),
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: Offset.fromDirection(getRadiansFromDegree(225),
+                        degTwoTranslationAnimation.value * 100),
+                    child: Transform(
+                      transform: Matrix4.rotationZ(
+                          getRadiansFromDegree(rotationAnimation.value))
+                        ..scale(degTwoTranslationAnimation.value),
+                      alignment: Alignment.center,
+                      child: CircularButton(
+                        color: Colors.black,
+                        width: 50,
+                        heigth: 50,
+                        icon: Icon(
+                          Icons.camera_alt,
+                          color: Colors.white,
+                        ),
+                        onclick: () {
+                          print('Second button');
+                        },
+                      ),
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: Offset.fromDirection(getRadiansFromDegree(180),
+                        degThreeTranslationAnimation.value * 100),
+                    child: Transform(
+                      transform: Matrix4.rotationZ(
+                          getRadiansFromDegree(rotationAnimation.value))
+                        ..scale(degThreeTranslationAnimation.value),
+                      alignment: Alignment.center,
+                      child: CircularButton(
+                        color: Colors.orangeAccent,
+                        width: 50,
+                        heigth: 50,
+                        icon: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                        ),
+                        onclick: () {
+                          print('Third Button');
+                        },
+                      ),
+                    ),
+                  ),
+                  Transform(
+                    transform: Matrix4.rotationZ(
+                        getRadiansFromDegree(rotationAnimation.value)),
+                    alignment: Alignment.center,
+                    child: CircularButton(
+                      color: Colors.red,
                       width: 60,
                       heigth: 60,
-                      color: Colors.red,
                       icon: Icon(
                         Icons.menu,
                         color: Colors.white,
                       ),
-                      onclick: () {})
+                      onclick: () {
+                        if (animationController!.isCompleted) {
+                          animationController!.reverse();
+                        } else {
+                          animationController!.forward();
+                        }
+                      },
+                    ),
+                  )
                 ],
-              ),
-            ),
-          ],
-        ),
+              ))
+        ],
       ),
-    );
+    ));
   }
 }
